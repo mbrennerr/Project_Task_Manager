8c12948dd2d260dffd2b469ac6ea0cac077364b6
mbrennerr <marciobrennerbusiness@gmail.com>
2021-06-27 03:24:14 -0300

    c3 - Requisito 10

--- script.js
+++ script.js
@@ -15,16 +15,32 @@ const requisito7 = () =>{
     const lis = document.getElementsByClassName(".tarefa");
     const ol = document.getElementById("lista-tarefas");
     
-        li.addEventListener("click", () =>{
+        ol.addEventListener("click", (event) =>{
             for (let index = 0; index < lis.length; index++) {
                 const li = lis[index];
+                li.classList.remove("selected");
             }
-            event.target.classList.toggle("selected");
+            event.target.classList.add("selected");
+        });
+        ol.addEventListener("dblclick", (event) =>{
+            for (let index =0; index < lis.length; index++){
+                const li = lis[index];
+            }
+            event.target.classList.toggle("completed");
         });
      
-};
+}
+
+const requisito10 = () =>{
+    const ol = document.getElementById("lista-tarefas");
+    const buttonErase = document.getElementById("apaga-tudo");
+    buttonErase.addEventListener("click", () => {
+        ol.innerHTML = null;
+    });
+}
 
 window.onload = () =>{
     requisito4();
     requisito7();
+    requisito10();
 }
